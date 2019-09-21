ade5874eea493a1ca70a02b5b05629b9688f0ed5
hiyoriaya <hiyoriaya@gmail.com>
2019-09-21 16:17:20 +0900

    PWA化 wip

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import Firebase from "./firebase" //追加
+import "./registerServiceWorker"; //追加
 
 
 Firebase.init(); //追加
